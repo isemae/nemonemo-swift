928588afc366a51f32000ed26a19a257b0fd5ab1
isemae <foxlis97@naver.com>
2024-02-04 10:57:18 +0900

    Asset: meow UI: Gradient

--- nemonemo/ContentView.swift
+++ nemonemo/ContentView.swift
@@ -8,31 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
-	@State private var squareScale = 20
+	@State private var squareScale = 30
 	@State private var squareSize = UIScreen.main.bounds.width
-	let allColors: [Color] = [.pink, .red, .orange, .yellow, .green, .mint, .teal, .cyan, .blue, .indigo, .purple, .brown, .gray]
+	let allColors: [Color] = [.pink, .red, .orange, .yellow, .green, .mint, .teal, .cyan, .blue, .indigo, .purple, .gray]
 	
 	var body: some View {
 		let columns = [GridItem](repeating: GridItem(.flexible(), spacing: 0), count: Int(squareScale))
 		ZStack {
-			Image(systemName: "trash")
+			Image("nemoCat")
+//			Image(systemName: "nemoCat")
+				.resizable()
+				.scaledToFit()
+				.background(.black)
 			LazyVGrid(columns: columns, spacing: 0) {
 				ForEach(0..<(Int(squareScale) * Int(squareScale) * 3)) { i in
 					ZStack {
 						Rectangle()
 							.frame(width: squareSize / CGFloat(squareScale), height: squareSize / CGFloat(squareScale))
-							.foregroundStyle(allColors.randomElement()!)
-							.foregroundStyle(.ultraThickMaterial)
-						Rectangle()
-							.foregroundStyle(.ultraThickMaterial)
-							.opacity(0.4)
+//							.foregroundColor(allColors.randomElement()!)
+							.foregroundStyle(.ultraThinMaterial)
+						
+//						RadialGradient(gradient: Gradient(colors: [.clear, .black]), center: .center, startRadius: 0, endRadius: squareSize / CGFloat(squareScale))
+						AngularGradient(gradient: Gradient(colors: [.secondary.opacity(0.5), .clear]), center: .center, startAngle: .degrees(-90), endAngle: .degrees(270))
+//							.blur(radius: squareSize / CGFloat(squareScale) / 10)
+//							.frame(width: squareSize / CGFloat(squareScale), height: squareSize / CGFloat(squareScale))
 					}
 				}
 			}
-			.opacity(0.5)
-		}
 			.frame(width: .infinity, height: .infinity)
-//		)
+		}
 	}
 }
 
