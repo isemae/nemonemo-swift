48d5b6d6fb399e2ad4c6b5504d9ae40c4e606a36
isemae <foxlis97@naver.com>
2024-02-14 01:49:21 +0900

    Chore: Views separated

--- nemonemo/ContentView.swift
+++ nemonemo/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
 	@State private var isEditing = false
 	@State private var isEmbossed = false
 	@State private var isPixeled = false
-
+	
 	@State private var avgColorsRow: [[ColorData]] = []
 	var image = UIImage(named: "punchCat")
 	var body: some View {
@@ -21,37 +21,49 @@ struct ContentView: View {
 			Image("punchCat")
 				.resizable()
 				.scaledToFit()
-
 			if isPixeled && !isEditing {
-				VStack(spacing: 0) {
-					ForEach(avgColorsRow, id: \.self) { row in
-						HStack(spacing: 0) {
-							ForEach(row, id: \.id) { colorTuple in
-								ZStack {
-									Rectangle()
-										.foregroundColor(Color(colorTuple.color))
-									if isEmbossed {
-										AngularGradient(gradient: Gradient(colors: [.secondary.opacity(0.5), .clear]), center: .center, startAngle: .degrees(-90), endAngle: .degrees(270))
-									}
-								}
+				PixelatedImageView()
+			}
+		}
+		.frame(width: image!.size.width, height: image!.size.height)
+		ImageProcessingToggles(isEmbossed: $isEmbossed, isPixeled: $isPixeled)
+		ScaleSlider(isEditing: $isEditing, squareScale: $squareScale)
+	}
+	
+	func PixelatedImageView() -> some View {
+		VStack(spacing: 0) {
+			ForEach(avgColorsRow, id: \.self) { row in
+				HStack(spacing: 0) {
+					ForEach(row, id: \.id) { colorTuple in
+						ZStack {
+							Rectangle()
+								.foregroundColor(Color(colorTuple.color))
+							if isEmbossed {
+								AngularGradient(gradient: Gradient(colors: [.secondary.opacity(0.5), .clear]), center: .center, startAngle: .degrees(-90), endAngle: .degrees(270))
 							}
 						}
 					}
 				}
-				.onChange(of: squareScale) {
-					DispatchQueue.global().async {
-						avgColorsRow = getCIImageContext(imageName: "punchCat", unitSize: squareScale)
-					}
+			}
+			.onChange(of: squareScale) {
+				DispatchQueue.global().async {
+					avgColorsRow = getCIImageContext(imageName: "punchCat", unitSize: squareScale)
 				}
-				.onAppear() {
-					DispatchQueue.global().async {
-						avgColorsRow = getCIImageContext(imageName: "punchCat", unitSize: squareScale)
-					}
+			}
+			.onAppear() {
+				DispatchQueue.global().async {
+					avgColorsRow = getCIImageContext(imageName: "punchCat", unitSize: squareScale)
 				}
 			}
 		}
-		.frame(width: image!.size.width, height: image!.size.height)
-		
+	}
+}
+
+struct ImageProcessingToggles: View {
+	@Binding var isEmbossed: Bool
+	@Binding var isPixeled: Bool
+	
+	var body: some View {
 		HStack {
 			Toggle(isOn: $isPixeled) {
 				Text("Pixelate")
@@ -60,7 +72,6 @@ struct ContentView: View {
 				Text("Emboss")
 			}
 		}
-		ScaleSlider(isEditing: $isEditing, squareScale: $squareScale)
 	}
 }
 
@@ -74,6 +85,7 @@ struct ScaleSlider: View {
 		Text("\(Int(squareScale))")
 	}
 }
-//#Preview {
-//	ContentView(isEditing: <#Binding<Bool>#>, squareScale: <#Binding<CGFloat>#>)
-//}
+
+#Preview {
+	ContentView()
+}
